6b276fdc56a143ab4208637bbb71446fdf678487
WPMaster <halczynskim@outlook.com>
2017-06-12 00:21:51 +0200

    Create tools.sh

--- tools.sh
+++ tools.sh
@@ -5,13 +5,14 @@
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
-BACKTITLE="LENOVO K6 NOTE BOOTLOADER UNLOCK SCRIPT BY WPMASTER"
+BACKTITLE="LENOVO K6 NOTE TOOLS BY WPMASTER"
 TITLE="WARNING!"
 MENU="This operation will erase user data. Before unlocking you have to install android tools"
 
 OPTIONS=(1 "Reboot into Bootloader"
          2 "Check the bootloader status"
-         3 "Unlock Bootloader")
+         3 "Unlock Bootloader"
+		 4 "Install TeamWin Recovery Project")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -29,28 +30,51 @@ case $CHOICE in
 			clear
 			echo "Rebooting into Bootloader..."
 			adb reboot bootloader
-			./script.sh
+			./tools.sh
             ;;
         2)
             echo "Check the bootloader status"
 			echo "Script will continue after 5 seconds"
 			fastboot -i 0x17ef oem device-info
 			sleep 5
-			./script.sh
+			./tools.sh
             ;;
         3)
-            echo "Unlock Bootloader"
+            echo "Unlock Bootloader..."
 			fastboot -i 0x17ef oem unlock-go
 			clear
 			
-			echo Completed, expect the end of the formatting process
-			echo userdata partition on your device!
+			echo "Completed, expect the end of the formatting process"
+			echo "userdata partition on your device!"
 			echo
-			echo Upon completion of the formatting process,
-			echo the device will automatically reboot in Android OS.
+			echo "Upon completion of the formatting process,"
+			echo "the device will automatically reboot in Android OS."
 			echo
-			echo Thanks
+			echo "Thanks"
 			sleep 7
 			clear
+			./tools.sh
             ;;
+		4)
+			echo "Install TWRP..."
+			
+			echo "Check Android device..."
+			adb devices
+			clear
+			
+			echo "Reboot into bootloader..."
+			adb reboot bootloader
+			clear
+			
+			echo "Flashing TWRP recovery..."
+			fastboot -i 0x17ef flash recovery recovery.img
+			clear
+			
+			echo "Reboot into TWRP..."
+			fastboot oem reboot-recovery
+			
+			echo "Install completed!"
+			sleep 3
+			clear
+			;;
 esac
